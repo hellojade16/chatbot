34b0bfcb9edb5f74b48394abfe9b27d2de3a4f28
hellojade16 <domingo.pjade16@gmail.com>
2025-04-29 12:18:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import Flask, render_template, request, jsonify
 from google import genai
 from google.genai import types
 
-port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-app.run(host="0.0.0.0", port=port)
-
 app = Flask(__name__)
 
 # Initialize the GenAI client
